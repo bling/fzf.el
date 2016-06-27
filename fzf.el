b75c86970dbd9f129361ba567f3ab9833382f4ee
David Sargeant <dsargeant@boomtownroi.com>
2016-06-27 10:42:48 -0400

    Window on the bottom.

--- fzf.el
+++ fzf.el
@@ -44,7 +44,7 @@
   "Configuration options for fzf.el"
   :group 'convenience)
 
-(defcustom fzf/window-height 15
+(defcustom fzf/window-height -10
   "The window height of the fzf buffer"
   :type 'integer
   :group 'fzf)
@@ -77,10 +77,13 @@
     (advice-add 'term-handle-exit :after #'fzf/after-term-handle-exit)
     (let ((buf (get-buffer-create "*fzf*")))
       (split-window-vertically fzf/window-height)
+      (other-window 1)
       (if fzf/args
           (apply 'make-term "fzf" fzf/executable nil (split-string fzf/args " "))
         (make-term "fzf" fzf/executable))
       (switch-to-buffer buf)
+      (linum-mode 0)
+      (set-window-margins nil 1)
 
       ;; disable various settings known to cause artifacts, see #1 for more details
       (setq-local scroll-margin 0)
