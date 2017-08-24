26585f9a66d6604b4a36598ed81df064e97ec6c0
Bailey Ling <bling@live.ca>
2017-08-24 11:56:40 -0400

    remove problematic window margins
    this causes a host of rendering problems

--- fzf.el
+++ fzf.el
@@ -105,7 +105,6 @@
       (make-term "fzf" fzf/executable))
     (switch-to-buffer buf)
     (linum-mode 0)
-    ;;(set-window-margins nil 1)
 
     ;; disable various settings known to cause artifacts, see #1 for more details
     (setq-local scroll-margin 0)
